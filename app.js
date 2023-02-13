8d91ff07c7a9bbd5b1cc4f45b7fe33dd04e4b14a
Denilson Junior <jrdenilson1@outlook.com>
2023-02-13 14:36:48 -0300

    Adding mongoDB

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let homePosts = [];
+//let homePosts = [];
 
 const app = express();
 
@@ -17,33 +18,51 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://localhost:27017/Blog');
 
+  const postsSchema = new mongoose.Schema({
+    title: String,
+    text: String
+  });
 
+  const Posts = mongoose.model("Posts", postsSchema);
+
+  
+  
 app.get("/", function (req, res) {
+
+Posts.find({}, function (err, posts) {
+
   res.render('home', {
     homeFirstContent: homeStartingContent,
-    compose: homePosts
+    compose: posts
    });
 })
 
-app.get("/posts/:postName", function (req, res) {
-  let postRoute = _.lowerCase(req.params.postName);
+})
+
+app.get("/posts/:postid", function (req, res) {
+  let requestedPostId = req.params.postid;
 
 
   
 
-  console.log(postRoute);
-  
-  homePosts.forEach(function (posts) {
-    const titleStorage = _.lowerCase(posts.postTitle)
-    if ( titleStorage === postRoute) {
-      res.render('post', {postsContent: posts.postContent,
-      postsTitle: posts.postTitle
+  Posts.findOne({_id: requestedPostId}, function (err, post) {
+    res.render('post', {
+      postsContent: post.text,
+      postsTitle: post.title
       })
-      } else {
-        console.log("Not Match");
-      }
   })
+  
+//  homePosts.forEach(function (posts) {
+//    const titleStorage = _.lowerCase(posts.postTitle)
+//    if ( titleStorage === postRoute) {
+//      
+//      } else {
+//        console.log("Not Match");
+//      }
+//  })
 })
 
 app.get("/about", function (req, res) {
@@ -57,20 +76,26 @@ app.get("/contact", function (req, res) {
 })
 
 app.get("/compose", function (req, res) {
+
   res.render('compose');
   
 })
 
 app.post("/compose", function (req, res) {
-  const blogPost = {
-    postTitle: req.body.newTitle,
-    postContent: req.body.newPost
-  }
+  const blogPost = new Posts({
+    title: req.body.newTitle,
+    text: req.body.newPost
+  })
 
-  homePosts.push(blogPost);
+  blogPost.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+  //homePosts.push(blogPost);
   
 
-  res.redirect("/");
+  
   
 })
 
